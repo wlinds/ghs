9b0fa51d9b1e12acc18a153c6cee749029fb4671
William L <41084946+helveticae@users.noreply.github.com>
2023-11-27 17:13:07 +0100

    headless selenium, add tqdm

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from bs4 import BeautifulSoup as bs4
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+from tqdm import tqdm
 
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
 WAIT_TIME = 15
@@ -12,7 +15,11 @@ def get_datetime_from_url(url):
     headers = {'User-Agent': USER_AGENT}
     response = requests.get(url, headers=headers)
 
-    with webdriver.Chrome() as driver:
+    # Running headless
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
+
+    with webdriver.Chrome(options=chrome_options) as driver:
         driver.get(url)
         driver.implicitly_wait(WAIT_TIME)
         content = driver.page_source
@@ -28,7 +35,7 @@ def get_datetime_from_url(url):
         
         # Idk why it doesn't find the message sometimes. Seems to work when retrying.
         if commit_message == "Error fetching commit msg":
-            print("Could not fetch commit message. Retrying...")
+            print(f"Could not fetch commit message for {url[19:url.find('/', 19)]}. Retrying...")
             d, c = get_datetime_from_url(url)
             return d, c
         
@@ -57,6 +64,7 @@ def create_csv(filename='data.csv'):
 def main():
     course = "n"
     urls = []
+    new_commits = 0
 
     # csv with urls to be tracked
     with open('urls.csv', 'r', newline='') as csvfile:
@@ -68,7 +76,9 @@ def main():
     if not file_exists:
         create_csv()
 
-    for url in urls:
+    progress_bar = tqdm(urls, desc="Processing URLs")
+
+    for url in progress_bar:
         username = url[19:url.find('/', 19)]
         datetime_value, commit_message = get_datetime_from_url(url)
 
@@ -80,14 +90,25 @@ def main():
                     if row['url'] == url and row['datetime'] == datetime_value:
                         break
                 else:
-                    print(f"New commit found: {url[18::]} | {commit_message}")
+                    tqdm.write(f"New commit found: {datetime_value}\n{url[18::]} | {commit_message}")
                     save_to_csv(course, url, datetime_value, commit_message, username)
+                    new_commits += 1
+
+        progress_bar.set_postfix(username=username)
         time.sleep(1)
+    
+    return new_commits
 
 if __name__ == "__main__":
     while True:
-        print(f"{datetime.datetime.now()} Running script")
-        main()
+        print(f"{datetime.datetime.now()} [+] Running script")
+        n_commits = main()
+        
+        if n_commits == 0:
+            print(f"No new updates.")
+        else:
+            print(f"{n_commits} new updates.\n")
+
         next_run_time = datetime.datetime.now() + datetime.timedelta(seconds=CHECK_INTERVAL_SECONDS)
         print(f"Next run: {next_run_time.strftime('%Y-%m-%d %H:%M:%S')}")
         time.sleep(CHECK_INTERVAL_SECONDS)
